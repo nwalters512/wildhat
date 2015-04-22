626908b4154c2e32a471271e9ef413df588ee483
Nathan Walters <nwalters512@gmail.com>
2015-04-21 22:10:41 -0500

    Removed legacy code, added rainbow function.

--- wildhat.ino
+++ wildhat.ino
@@ -1,19 +1,16 @@
 #include <FastLED.h>
 
 #define NUM_LEDS 44
-
 #define DATA_PIN    10
 #define CLOCK_PIN   11
+#define BRIGHTNESS 32
 
 CRGB leds[NUM_LEDS];
 
 void setup() {
 
   FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN>(leds, NUM_LEDS);
-
-  //strip.begin(); // Initialize pins for output
-  //strip.show();  // Turn all LEDs off ASAP
-  //strip.setBrightness(50);
+  FastLED.setBrightness(BRIGHTNESS);
 
   Serial.begin(9600);
 }
@@ -25,9 +22,9 @@ int      head  = 0, tail = -10; // Index of first 'on' and 'off' pixels
 uint32_t color = 0xFF0000;      // 'On' color (starts red)
 
 void loop() {
-  //rainbowCycle(0);
-  bounceSegment(3, 4, CRGB::Red);
-  delay(2000);
+  bounceSegment(5, 5, CRGB::Red);
+  bounceSegment(5, 5, CRGB::Blue);
+  rainbowCycle(5, 10);
 }
 
 // Bounces a segment of LEDs from one end of the strip to another
@@ -67,51 +64,20 @@ void bounceSegment(int segmentLength, uint8_t numBounces, CRGB color) {
 
     FastLED.show();
 
-    delay(20);
+    delay(25);
   }
 }
 
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-
-  uint16_t i, j;
-
-  for(j = 0; j < 256*5; j++) { // 5 cycles of all colors on wheel
-    for(i = 0; i < NUM_LEDS; i++) {
-      uint32_t color = Wheel(((i * 256 / NUM_LEDS) + j) & 255);
-      leds[i].r = (uint8_t)(color >> 16);
-      leds[i].g = (uint8_t)(color >>  8);
-      leds[i].b = (uint8_t)color;
+// Animates a nainbow in a circle around the hat
+void rainbowCycle(int cycles, uint8_t wait) {  
+  for(int j = 0; j < 256*cycles; j++) {
+    for(int i = 0; i < NUM_LEDS; i++) {
+      // Map to a hue in the range 0-255
+      // This will get us a color on the rainbow
+      leds[i] = CHSV((i * 256 / NUM_LEDS) + j & 255, 255, 255);
     }
     FastLED.show();
     delay(wait);
-  }
-
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  CRGB color;
-  if(WheelPos < 85) {
-    color.r = WheelPos * 3;
-    color.g = 255 - WheelPos * 3;
-    color.b = 0;
-    return color;
-  } 
-  else if(WheelPos < 170) {
-    WheelPos -= 85;
-    color.r = 255 - WheelPos * 3;
-    color.g = 0;
-    color.b = WheelPos * 3;
-    return color;
-  } 
-  else {
-    WheelPos -= 170;
-    color.r = 0;
-    color.g = WheelPos * 3;
-    color.b = 255 - WheelPos * 3;
-    return color;
   }
 }
 
