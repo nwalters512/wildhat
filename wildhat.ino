148ea758d1a660b4afc3cb2b0f6fd7daacb324aa
Nathan Walters <nwalters512@gmail.com>
2015-04-21 22:15:48 -0500

    Added comments, removed more legacy code.

--- wildhat.ino
+++ wildhat.ino
@@ -5,21 +5,15 @@
 #define CLOCK_PIN   11
 #define BRIGHTNESS 32
 
+// Set up LED strip
 CRGB leds[NUM_LEDS];
 
 void setup() {
-
+  // I used APA102-based LEDs; change this as needed to match your setup
   FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN>(leds, NUM_LEDS);
+  // Brightness is limited to conserve power
   FastLED.setBrightness(BRIGHTNESS);
-
-  Serial.begin(9600);
 }
-
-// Runs 10 LEDs at a time along strip, cycling through red, green and blue.
-// This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
-
-int      head  = 0, tail = -10; // Index of first 'on' and 'off' pixels
-uint32_t color = 0xFF0000;      // 'On' color (starts red)
 
 void loop() {
   bounceSegment(5, 5, CRGB::Red);
@@ -29,32 +23,36 @@ void loop() {
 
 // Bounces a segment of LEDs from one end of the strip to another
 void bounceSegment(int segmentLength, uint8_t numBounces, CRGB color) {
-  // True is moving low to high, false is moving high to low
 
   int segStart = 0;
+  // True is moving low to high, false is moving high to low
   int segDirection = true;
 
   int numBouncesCounted = 0;
 
   while(numBouncesCounted < numBounces) {
 
+    // Initialize all LEDs to off
     for(int i = 0; i < NUM_LEDS; i++) {
       leds[i] = 0;
     }
 
+    // Turn on LEDs that should be lit this cycle
     for(int i = segStart; i < segStart + segmentLength; i++) {
       leds[i] = color;
     }
 
+    // If we reached either end of the strip, reverse direction
     if(segStart + segmentLength >= NUM_LEDS && segDirection == true) {
       segDirection = false;
     } 
     else if (segStart == 0 && segDirection == false) {
       segDirection = true;
-      // one bounce completed
+      // We're back at the start of the strip; increment bounce count
       numBouncesCounted++;
     }
 
+    // Move the start of the segment in the appropriate direction
     if(segDirection) {
       segStart++;
     } 
@@ -62,8 +60,8 @@ void bounceSegment(int segmentLength, uint8_t numBounces, CRGB color) {
       segStart--;
     }
 
+    // Flush to the strip and delay to give a nice animation (roughly 40 FPS)
     FastLED.show();
-
     delay(25);
   }
 }
@@ -76,10 +74,13 @@ void rainbowCycle(int cycles, uint8_t wait) {
       // This will get us a color on the rainbow
       leds[i] = CHSV((i * 256 / NUM_LEDS) + j & 255, 255, 255);
     }
+
+    // Flush to the strip and delay to give a nice animation (roughly 40 FPS)
     FastLED.show();
     delay(wait);
   }
 }
+
 
 
 
